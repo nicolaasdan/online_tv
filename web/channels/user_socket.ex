1f1eb07be284ff279a3a03ba03c14ac8eed726c0
nicolaasdan <nicolaasdanneels@MacBook-Pro-van-Nicolaas.local>
2017-04-22 14:48:20 +0200

    check origin fix in usersocket

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -5,7 +5,9 @@ defmodule OnlineTv.UserSocket do
   channel "room", OnlineTv.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket,
+    timeout: 45_000,
+    check_origin: false
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
