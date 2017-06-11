19d1ddc4bba42b6472805d76a201979953bb5969
nicolaasdan <nicolaasdanneels@MacBook-Pro-van-Nicolaas.local>
2017-06-11 16:10:22 +0200

    v0.1

--- web/static/js/timezone.js
+++ web/static/js/timezone.js
@@ -15,3 +15,13 @@ $('.mytz').each(function(index, value) {
 });
 
 
+$('.gmt').each(function(index, value) {
+	var d = new Date();
+	var int = parseInt($(this).text())
+	var hour = int + currentTimeZoneOffsetInHours
+	if(hour > 24) {
+		hour = hour - 24
+	}
+	d.setHours(hour)
+	$(this).text(d.getHours())
+});
